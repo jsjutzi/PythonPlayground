e3110e60e206e0ed0c35971b75b5046a2d938863
Jack Jutzi <jsjutzi@gmail.com>
2020-12-27 09:58:36 -0600

    add input/output logic

---
+++ input_output.py
@@ -0,0 +1,27 @@
+# jabber = open("/Users/jackjutzi/sample.txt", 'r')
+#
+# for line in jabber:
+#     if "jabberwock" in line.lower():
+#         print(line)
+#
+# jabber.close()
+
+with open("/Users/jackjutzi/sample.txt", 'r') as jabber:
+    line = jabber.readline()
+    while line:
+        print(line, end='')
+        line = jabber.readline()
+
+cities = []
+
+with open("/Users/jackjutzi/cities.txt") as city_file:
+    for city in city_file:
+        cities.append(city.strip('\n'))
+
+print(cities)
+
+with open("/Users/jackjutzi/sample.txt", 'a') as jabber:
+    for i in range(1, 13):
+        result = i*2
+        print("{0} times 2 is {1}".format(i, result), file=jabber)
+    print("-"*20, file=jabber)
--- pythonConcepts/dicts.py
+++ pythonConcepts/dicts.py
@@ -14,6 +14,31 @@ fruits = {"apple": "sweet, crunchy fruit", "banana": "soft, yellow fruit", "lemo
 #     else:
 #         print("No such fruit recognized")
 
-print(fruits.keys())
-print(fruits.values())
-print(fruits.items())
+# print(fruits.keys())
+# print(fruits.values())
+# print(fruits.items())
+
+#Sets
+# farm_animals = {"sheep", "cow", "hen"}
+# print(farm_animals)
+#
+# for animal in farm_animals:
+#     print(animal)
+#
+# print("="*40)
+#
+# wild_animals = set(["lion", "tiger", "panther", "elephant", "hare"])
+# print(wild_animals)
+#
+# for animal in wild_animals:
+#     print(animal)
+#
+# farm_animals.add("horse")
+# wild_animals.add("horse")
+# print(farm_animals)
+# print(wild_animals)
+#
+# even = set(range(0, 40, 2))
+# print(even)
+# square_tuple = (4, 6, 9, 16, 25)
+
