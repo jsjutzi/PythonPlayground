5df77368f98b2bf3447b89dc27f802b88f667302
Jack Jutzi <jsjutzi@gmail.com>
2020-12-29 21:19:42 -0600

    convert to game character classes

--- oop/oop_playground.py
+++ oop/oop_playground.py
@@ -1,16 +1,27 @@
-class Kettle(object):
-    def __init__(self, make, price):
-        self.make = make
-        self.price = price
-        self.on = False
 
-# Empty comment
-kenwood = Kettle("Kenwood", 8.99)
-print(kenwood.make)
-print(kenwood.price)
-kenwood.price = 12.80
-print(kenwood.price)
+class Character(object):
+    def __init__(self, char_class, attack_power, hit_points):
+        self.char_class = char_class
+        self.attack_power = attack_power
+        self.hit_points = hit_points
+
+    def take_damage(self, damage):
+        self.hit_points = self.hit_points - damage
+
+    def gain_health(self, health):
+        self.hit_points = self.hit_points + health
+
+    def attack(self, target: object):
+        target.take_damage(self.attack_power)
 
 
-hamilton = Kettle("Hamilton", 14.55)
-print(hamilton.price)
+mage = Character("mage", 30, 150)
+knight = Character("mage", 40, 200)
+
+# Mage attacks Knight
+print(knight.hit_points)
+mage.attack(knight)
+print(knight.hit_points)
+
+
+
